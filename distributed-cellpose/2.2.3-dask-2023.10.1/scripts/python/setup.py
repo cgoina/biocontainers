4e6285d6ed43efd63a6825cc6db2531615b46821
Cristian Goina <goinac@hhmi.org>
2023-11-01 20:39:57 -0400

    renamed version folder

--- distributed-cellpose/2.2.3-dask-2023.10.1/scripts/python/setup.py
+++ distributed-cellpose/2.2.3-dask-2023.10.1/scripts/python/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="distributed_cellpose",
-    version="2.0.0",
+    version="2.2.3",
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
--- distributed-cellpose/2.2.3/scripts/python/distsegutils/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__= [
-    "io_utils",
-    "n5_utils"
-]
